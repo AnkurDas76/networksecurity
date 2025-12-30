e0713c914587900994cc022a4a4e0a239cd7b2f2
AnkurDas76 <ankurdas7602@gmail.com>
2025-12-30 12:31:54 +0530

    debugging

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -25,39 +25,38 @@ from networksecurity.entity.artifact_entity import (
 )
 
 
-
 from networksecurity.constant.training_pipeline import SAVED_MODEL_DIR
 import sys
 
 
 class TrainingPipeline:
     def __init__(self):
-        self.training_ppeline_config=TrainingPipelineConfig()
+        self.training_pipeline_config=TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
+        
 
     def start_data_ingestion(self):
         try:
-            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_ppeline_config)
-            logging.info('started data ingestion')
+            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Start data Ingestion")
             data_ingestion=DataIngestion(data_ingestion_config=self.data_ingestion_config)
             data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
             logging.info(f"Data Ingestion completed and artifact: {data_ingestion_artifact}")
             return data_ingestion_artifact
-
+        
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_data_validation(self,data_ingestion_artifact: DataIngestionArtifact):
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
         try:
-            data_validation_config=DataValidationConfig(training_pipeline_config=self.training_ppeline_config)
+            data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
             data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
-            logging.info("Initiated the data validation")
+            logging.info("Initiate the data Validation")
             data_validation_artifact=data_validation.initiate_data_validation()
             return data_validation_artifact
-
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    
     def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
         try:
             data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
@@ -87,7 +86,10 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
-
+    
+        
+    
+    
     def run_pipeline(self):
         try:
             data_ingestion_artifact=self.start_data_ingestion()
@@ -95,9 +97,10 @@ class TrainingPipeline:
             data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             
-            self.sync_artifact_dir_to_s3()
-            self.sync_saved_model_dir_to_s3()
+            
             
             return model_trainer_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    
